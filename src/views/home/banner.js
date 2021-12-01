529819caa615e3f16d30f9ad293f156e5c4c76a6
TMINS <1714316336@qq.com>
2021-12-01 16:27:16 +0800

    增加动画

---
+++ src/views/home/banner.js
@@ -0,0 +1,67 @@
+export default [
+    {
+        img: require('@/assets/product1.png'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product2.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product3.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product4.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product5.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product6.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product7.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product8.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product9.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product10.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product11.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product12.jpg'),
+        active: false,
+        like: false,
+    },
+    {
+        img: require('@/assets/product13.jpg'),
+        active: false,
+        like: false,
+    },
+]
